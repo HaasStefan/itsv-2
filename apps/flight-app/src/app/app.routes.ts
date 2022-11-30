e5d10d6ccc08ead8dd763f408a8ac54040779a6f
Stefan Haas <stefan.haas.privat@gmail.com>
2022-11-30 11:54:08 +0100

    module federation

--- apps/flight-app/src/app/app.routes.ts
+++ apps/flight-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { BasketComponent } from './basket/basket.component';
 import { HomeComponent } from './home/home.component';
+import {loadRemoteModule} from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
   {
@@ -16,6 +17,16 @@ export const APP_ROUTES: Routes = [
     path: 'basket',
     component: BasketComponent,
     outlet: 'aux'
+  },
+  {
+    path: 'passenger',
+    loadChildren: () => 
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:3000/remoteEntry.js',
+      exposedModule: './module'
+    }).then(m => m.PassengerModule)
+    //  import('passenger/Module').then(m => m.PassengerModule)
   },
   {
     path: '**',
